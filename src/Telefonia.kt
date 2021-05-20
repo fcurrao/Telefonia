bb58b9fb751c454cf48fe5509e11517366265535
fcurrao <fecurrao@alumno.unlam.edu.ar>
2021-05-20 20:30:44 -0300

    subiendooo

---
+++ src/Telefonia.kt
@@ -0,0 +1,66 @@
+public class Telefonia {
+
+
+     class Llamada(
+        var codigo_cliente: Int,
+        var fecha_llamada: String,
+        var hora_llamada: String,
+        var duracion_llamada: Double,
+        var tipo_llamada: Char
+    ) {
+
+    }
+
+
+
+
+    class Costo() extends Llamada {
+     val llamada_finde
+     val llamada_noche
+     val llamada_regular
+
+     if ()
+
+    }
+
+
+    fun factura_total_por_cliente() {
+        this.conseguir_cliente()
+    }
+
+    fun historial_de_llamadas() {
+
+    }
+
+    fun alta_cliente() {
+    }
+
+        fun agregarCliente(nombreCliente: String, password: String): Boolean {
+
+            codigoC
+            if (noExisteElNombreUsuario(codigo_cliente)) {
+                Cliente.add(Cliente(codigo_cliente))
+                return true
+            }
+            return false
+        }
+
+
+        private fun noExisteElNombreUsuario(nombreUsuario: String) =
+            nombresUsuarioExistentes.add(nombreUsuario)
+
+    }
+
+    fun consultar_costo() {
+
+    }
+
+    fun costo_total_de_todos_los_clientes() {
+
+    }
+}
+
+
+/*
+
+*/
