2c59619b34067a4f0954416a7acf03f5893fe8d6
fcurrao <fecurrao@alumno.unlam.edu.ar>
2021-05-20 20:31:55 -0300

    sub

---
+++ src/Llamada.kt
@@ -0,0 +1,10 @@
+public class Cliente {
+
+    public class Cliente  (codigo_cliente: Int) {
+    val nombre_cliente: String = ""
+    val apellido_cliente: String = ""
+    val fechadealta_cliente: String = ""
+
+
+ }
+}
