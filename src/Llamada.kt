ed5ddb43c8f934b30a2da195f1cf3c2af6d7bf66
fcurrao <68132577+fcurrao@users.noreply.github.com>
2021-05-21 10:34:23 -0300

    Update Llamada.kt

--- src/Llamada.kt
+++ src/Llamada.kt
@@ -1,4 +1,9 @@
-public class Llamada {
+
+        import java.util.Formatter.*;
+
+
+
+class Llamada extends Cliente(String nombre, int codigoCliente; byte tipoCliente) {
 
     public class Llamada  (codigo_cliente: Int) {
     val
@@ -6,7 +11,24 @@ public class Llamada {
     val nombre_cliente: String = ""
     val apellido_cliente: String = ""
     val fechadealta_cliente: String = ""
+          int codigoLlamada;	
+  time horaDeLlamada;
+  boolean finDeSemana;
+  int claseDeLlamadaInternONacional;
+  double minutosDeLlamada;
 
+  
+
+  
+  	
+  
+  
+  public static void main(String[] args) {
+      
+    System.out.println("Hello Codiva");
+    
+  }
+}
 
  }
 }
